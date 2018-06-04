372c3b24199f1e9a253371515c92eae379fe7497
driftfloat <fallwater@126.com>
2018-06-04 21:28:02 +0800

    change to mvn

---
+++ src/main/java/com/fallwater/testSpringMvc/entity/User.java
@@ -0,0 +1,63 @@
+package com.fallwater.testSpringMvc.entity;
+
+public class User {
+    private Integer id;
+
+    private String username;
+
+    private String phone;
+
+    private String email;
+
+    private String status;
+
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
